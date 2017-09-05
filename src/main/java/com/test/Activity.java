0496d0da2185d2f47ce75c3773e2ed4462d94a0d
wel <noeloariola@gmail.com>
2017-09-05 23:48:46 +0800

    test

---
+++ src/main/java/com/test/Activity.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Activity {
+	public static void main(String[] args) {
+		for(int i = 1 ; i < 6; i++){
+			for(int x = i; x < i; x++) {
+				System.out.print(x);
+			}
+			System.out.println();
+			
+		}
+	}
+	
+}
